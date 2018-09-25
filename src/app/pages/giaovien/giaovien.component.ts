344e8e15f102cb00b1f01529b49c771d7cadc37c
Tran Hoang Minh <angelsea951@gmail.com>
2018-09-25 10:49:23 +0700

    update

---
+++ src/app/pages/giaovien/giaovien.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giaovien',
+  templateUrl: './giaovien.component.html',
+  styleUrls: ['./giaovien.component.scss']
+})
+export class GiaovienComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
